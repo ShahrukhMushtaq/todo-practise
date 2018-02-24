6a169c451ef4cd8026e2990bcab78e3811c5f0f4
ShahrukhMushtaq <shahrukhmushtaq23@gmail.com>
2018-02-25 01:32:17 +0500

    Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'MY TODO APP';
+  todolist = [{description : 'Do something',}];
+
+  addTodo(newTodo){
+    var newList = {description : newTodo};
+    this.todolist.push(newList);
+
+  }
+
+  deleteTodo(todo){
+    this.todolist = this.todolist.filter(t => t.description !== todo.description);
+
+  }
+private edit: boolean = false;
+editTodo(){
+  this.edit = true;
+}
+
+ngOnChanges(){
+  this.edit = false;
+}
 }
